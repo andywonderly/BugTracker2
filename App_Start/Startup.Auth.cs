f9bfa74ead50ed6f0618660f7ff3983f7f3c5820
Andrew Wonderly <andywonderly@gmail.com>
2016-05-10 17:46:13 -0400

    Update Startup.Auth.cs

--- App_Start/Startup.Auth.cs
+++ App_Start/Startup.Auth.cs
@@ -1,1 +1,2 @@
-
+The Facebook and Google app IDs and app secrets have been reset.  I mistakenly added this file to the repository in the same
+commit as the update to my git ignore, so it was published.
