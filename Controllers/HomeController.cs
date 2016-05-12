4b8d43b4dd3893b3b874af678612dae2a2d8bba0
Andrew Wonderly <andywonderly@gmail.com>
2016-05-12 11:20:22 -0400

    Dashboard
    Started dashboard stuff.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BugTracker2.Models;
+using BugTracker2.Models.Helpers;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +11,35 @@ namespace BugTracker2.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
-            return View();
+            DashboardViewModel dashboardViewModel = new DashboardViewModel();
+            var currentUserId = System.Web.HttpContext.Current.User.Identity.GetUserId();
+            ApplicationUser currentUser = db.Users.Find(currentUserId);
+            ProjectUsersHelper projectUsersHelper = new ProjectUsersHelper();
+
+            dashboardViewModel.UserId = currentUser.Id;
+            dashboardViewModel.UserDisplayName = currentUser.DisplayName;
+            dashboardViewModel.UserAssignments = db.Tickets.Where(n => n.AssignedToUserId == currentUserId).ToList();
+            dashboardViewModel.UserProjects = new List<Project>();
+            dashboardViewModel.UserTickets = new List<Ticket>();
+
+            List<Project> allProjects = db.Projects.ToList();
+
+            foreach(var item in allProjects)
+                foreach (var item2 in item.ProjectUsers)
+                    if (item2.Id == currentUserId)
+                        dashboardViewModel.UserProjects.Add(item);
+
+            List<Ticket> allTickets = db.Tickets.ToList();
+
+            foreach (var item in allTickets)
+                if (item.OwnerUserId == currentUserId)
+                    dashboardViewModel.UserTickets.Add(item);
+            
+            return View(dashboardViewModel);
         }
 
         public ActionResult About()
---
+++ Models/DashboardViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTracker2.Models
+{
+    public class DashboardViewModel
+    {
+        public string UserId { get; set; }
+        public string UserDisplayName { get; set; }
+        public List<Ticket> UserAssignments { get; set; }
+        public List<Project> UserProjects { get; set; }
+        public List<Ticket> UserTickets { get; set; }
+    }
+}
